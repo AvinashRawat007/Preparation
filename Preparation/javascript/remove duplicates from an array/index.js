a38b16a0811d63ce4b3d69ab4575f98f1c3e0284
AvinashRawat007 <71925845+AvinashRawat007@users.noreply.github.com>
2022-11-04 00:14:33 +0530

    Add files via upload

--- Preparation/javascript/remove duplicates from an array/index.js
+++ Preparation/javascript/remove duplicates from an array/index.js
@@ -1,13 +1,13 @@
-var arr = [7,5,4,3,3,2,7,4];
+var arr = [7,5,4,3,3,2,7,4,5];
 
 function removeDuplicates(arr) {
-var unique = [];
+var arr1 = [];
 for(i=0; i < arr.length; i++){ 
-    if(unique.indexOf(arr[i]) === -1) { 
-        unique.push(arr[i]); 
+    if(arr1.indexOf(arr[i]) === -1) { 
+        arr1.push(arr[i]); 
     } 
 }
-return unique;
+return arr1;
 }
 
 console.log(removeDuplicates(arr));
